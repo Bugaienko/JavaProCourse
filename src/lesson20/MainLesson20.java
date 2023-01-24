ec55007faaf58a092643ef07f5ffbd4a55df3fda
Bugaienko <sergiibugaienko@gmail.com>
2023-01-24 14:39:50 +0100

    Create MainLesson20.java

---
+++ src/lesson20/MainLesson20.java
@@ -0,0 +1,14 @@
+package lesson20;
+
+/**
+ * Java Pro. Lesson #20.
+ *
+ * @author Sergii Bugaienko
+ * @version 23.01.23
+ */
+
+public class MainLesson20 {
+    public static void main(String[] args) {
+
+    }
+}
