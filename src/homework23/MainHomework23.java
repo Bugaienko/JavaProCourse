8960dc4792a55b5ecc4874d2d64257ce770f5a44
Bugaienko <sergiibugaienko@gmail.com>
2023-02-05 17:27:57 +0100

    Homework23 add

--- src/homework23/MainHomework23.java
+++ src/homework23/MainHomework23.java
@@ -28,22 +28,43 @@ public class MainHomework23 {
         Thread t1 = new Thread(() -> printer.print(doc1));
         Thread t2 = new Thread(() -> printer.print(doc2));
         Thread t3 = new Thread(() -> printer.print(doc3));
-        Thread t4 = new Thread(() -> printer.print(doc3));
+//        Thread t4 = new Thread(() -> printer.print(doc3));
 
 
         t1.start();
         t2.start();
         t3.start();
-        t4.start();
 
         t1.join();
         t2.join();
         t3.join();
-        t4.join();
 
         System.out.println();
         System.out.println("В очередь на печать отправленны страницы:");
         System.out.println(printer.getQuery());
+
+        // Version2 with ArrayBlockingQueue
+
+        PrinterQueue printer2 = new PrinterQueue();
+        Thread t5 = new Thread(() -> printer2.print(doc1));
+        Thread t6 = new Thread(() -> printer2.print(doc2));
+        Thread t7 = new Thread(() -> printer2.print(doc3));
+        Thread output = new Thread(() -> printer2.output());
+
+        t5.start();
+        t6.start();
+        t7.start();
+        output.start();
+
+
+        t5.join();
+        t6.join();
+        t7.join();
+        output.join();
+
+        System.out.println();
+        System.out.println("В очередь на печать отправленны страницы:");
+        System.out.println(printer2.getOutput());
 
     }
 }
---
+++ src/homework23/PrinterQueue.java
@@ -0,0 +1,46 @@
+package homework23;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author Sergii Bugaienko
+ */
+
+public class PrinterQueue {
+    private BlockingQueue<List<String>> queue = new ArrayBlockingQueue<>(5);
+    private List<String> output = new ArrayList<>();
+
+    public synchronized void print(List<String> list) {
+        try {
+            queue.put(list);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void output() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        while (!queue.isEmpty()) {
+            try {
+                List<String> task = queue.take();
+                for (String str: task) {
+                    output.add(str);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<String> getOutput() {
+        return output;
+    }
+}
