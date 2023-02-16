cf8a8785899288fbbf60e714cded512c91ddadb6
Bugaienko <sergiibugaienko@gmail.com>
2023-02-16 19:17:08 +0100

    gitignore add

---
+++ src/lesson27/MainLesson27.java
@@ -0,0 +1,14 @@
+package lesson27;
+
+/**
+ * JavaPro. Lesson #27
+ *
+ * @author Sergii Bugaienko
+ * @version 16.02.23
+ */
+
+public class MainLesson27 {
+    public static void main(String[] args) {
+
+    }
+}
